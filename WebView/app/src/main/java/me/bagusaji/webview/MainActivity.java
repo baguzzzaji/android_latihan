d34ed80d9bba105d9c3203e3fbfc11c3a1a88269
Bagus Aji Santoso <baguzzzaji@gmail.com>
2017-08-25 11:49:48 +0700

    Add WebView example

---
+++ WebView/app/src/main/java/me/bagusaji/webview/MainActivity.java
@@ -0,0 +1,34 @@
+package me.bagusaji.webview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        // Tiga baris di bawah ini agar laman yang dimuat dapat
+        // melakukan zoom.
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        // Baris di bawah untuk menambahkan scrollbar di dalam WebView-nya
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        // Memuat halaman web eksternal
+        webView.loadUrl("https://www.codepolitan.com");
+        // Memuat halaman web internal
+        // webView.loadUrl(Uri.parse("file:///android_asset/contoh.html").toString());
+    }
+}
